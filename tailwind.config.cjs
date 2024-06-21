dc83bbf834e8c4e5ef561de1d1f71f391642d3e5
Danny Cherry <dcherry@outlook.com>
2024-06-21 14:19:10 -0500

    updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}',
+            '../public/*.js',
+  ],
   theme: {
     extend: {
       colors: {
